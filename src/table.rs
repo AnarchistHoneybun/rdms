6ef8d62bd58e8ca11d3a89eb3e219cb67f369b1a
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-28 10:22:05 +0530

    stash work

--- src/table.rs
+++ src/table.rs
@@ -1,11 +1,35 @@
 use crate::column::{Column, ColumnDataType, Value};
 use std::{fmt, collections::HashSet};
 
+#[derive(Debug, PartialEq)]
+enum Operator {
+    Equal,
+    LessThan,
+    GreaterThan,
+    LessThanOrEqual,
+    GreaterThanOrEqual,
+}
+
+impl Operator {
+    fn from_str(s: &str) -> Result<Operator, String> {
+        match s {
+            "=" => Ok(Operator::Equal),
+            "<" => Ok(Operator::LessThan),
+            ">" => Ok(Operator::GreaterThan),
+            "<=" => Ok(Operator::LessThanOrEqual),
+            ">=" => Ok(Operator::GreaterThanOrEqual),
+            _ => Err(format!("Invalid operator: {}", s)),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum Error {
     MismatchedColumnCount,
     ParseError(usize, String),
     NonExistingColumns(Vec<String>),
+    NonExistingColumn(String), // column_name
+    InvalidOperator(String), // operator_str
 }
 
 impl fmt::Display for Error {
@@ -14,6 +38,8 @@ impl fmt::Display for Error {
             Error::MismatchedColumnCount => write!(f, "Number of values doesn't match the number of columns"),
             Error::ParseError(index, value) => write!(f, "Failed to parse value '{}' at index {}", value, index),
             Error::NonExistingColumns(columns) => write!(f, "The following columns do not exist: {}", columns.join(", ")),
+            Error::NonExistingColumn(column_name) => write!(f, "The column '{}' does not exist", column_name),
+            Error::InvalidOperator(operator_str) => write!(f, "Invalid operator: {}", operator_str),
         }
     }
 }
@@ -102,6 +128,52 @@ impl Table {
 
         for (column, value) in self.columns.iter_mut().zip(parsed_values.into_iter()) {
             column.data.push(value);
+        }
+
+        Ok(())
+    }
+
+    pub fn update(&mut self, update_input: (String, String), condition_input: Option<(String, String, String)>) -> Result<(), Error> {
+        // Validate column name in update_input
+        let update_column = self.columns.iter().find(|c| c.name == update_input.0).ok_or(Error::NonExistingColumn(update_input.0.clone()))?;
+
+        // Parse new_value according to the column's data type
+        let new_value = match update_column.data_type {
+            ColumnDataType::Integer => update_input.1.parse::<i64>().map(Value::Integer).map_err(|_| Error::ParseError(1, update_input.1.clone()))?,
+            ColumnDataType::Float => update_input.1.parse::<f64>().map(Value::Float).map_err(|_| Error::ParseError(1, update_input.1.clone()))?,
+            ColumnDataType::Text => Value::Text(update_input.1),
+        };
+
+        let update_column_name = self.columns.iter().find(|c| c.name == update_input.0).ok_or(Error::NonExistingColumn(update_input.0.clone()))?.name.clone();
+
+
+        if let Some((cond_column_name, cond_value, operator_str)) = condition_input {
+
+            // Validate condition column name
+            let cond_column = self.columns.iter().find(|c| c.name == cond_column_name).ok_or(Error::NonExistingColumn(cond_column_name))?;
+
+            // Parse the operator
+            let operator = Operator::from_str(&operator_str).map_err(|e| Error::InvalidOperator(operator_str))?;
+
+            // Update records based on the condition
+            for record in &mut self.columns {
+                if record.name == update_column_name {
+                    record.data = record.data.iter().enumerate().filter_map(|(i, value)| {
+                        if satisfies_condition(value, cond_column, &cond_value, &operator) {
+                            Some(new_value.clone())
+                        } else {
+                            Some(value.clone())
+                        }
+                    }).collect();
+                }
+            }
+        } else {
+            // Update all records with the new_value
+            for record in &mut self.columns {
+                if record.name == update_input.0 {
+                    record.data = vec![new_value.clone(); record.data.len()];
+                }
+            }
         }
 
         Ok(())
@@ -254,5 +326,35 @@ impl Table {
             print!("{} ", padded_data_type);
         }
         println!();
+    }
+}
+
+fn satisfies_condition(value: &Value, cond_column: &Column, cond_value: &str, operator: &Operator) -> bool {
+    match (value, &cond_column.data_type) {
+        (Value::Integer(val), ColumnDataType::Integer) => {
+            let cond_value: i64 = cond_value.parse().unwrap();
+            match operator {
+                Operator::Equal => val == &cond_value,
+                Operator::LessThan => val < &cond_value,
+                Operator::GreaterThan => val > &cond_value,
+                Operator::LessThanOrEqual => val <= &cond_value,
+                Operator::GreaterThanOrEqual => val >= &cond_value,
+            }
+        }
+        (Value::Float(val), ColumnDataType::Float) => {
+            let cond_value: f64 = cond_value.parse().unwrap();
+            match operator {
+                Operator::Equal => val == &cond_value,
+                Operator::LessThan => val < &cond_value,
+                Operator::GreaterThan => val > &cond_value,
+                Operator::LessThanOrEqual => val <= &cond_value,
+                Operator::GreaterThanOrEqual => val >= &cond_value,
+            }
+        }
+        (Value::Text(val), ColumnDataType::Text) => match operator {
+            Operator::Equal => val == cond_value,
+            _ => false, // Other operators not supported for Text data type
+        },
+        _ => false, // Unsupported data type or value combination
     }
 }
