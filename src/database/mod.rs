d7cbf333565d5946209d70741e4003f61245b050
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-05-13 10:21:53 +0530

    funcs to get table from db, add table to db etc

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,16 +1,18 @@
 mod errors;
 
-use std::collections::HashMap;
 use crate::database::errors::Error;
 use crate::table::Table;
+use std::collections::HashMap;
 
 pub struct Database {
+    pub name: String,
     pub tables: HashMap<String, Table>,
 }
 
 impl Database {
-    pub fn new() -> Self {
+    pub fn new(name: String) -> Self {
         Self {
+            name,
             tables: HashMap::new(),
         }
     }
@@ -23,5 +25,13 @@ impl Database {
 
         self.tables.insert(table.name.clone(), table);
         Ok(())
+    }
+
+    pub fn get_table(&self, table_name: &str) -> Option<&Table> {
+        self.tables.get(table_name)
+    }
+
+    pub fn get_table_mut(&mut self, table_name: &str) -> Option<&mut Table> {
+        self.tables.get_mut(table_name)
     }
 }
